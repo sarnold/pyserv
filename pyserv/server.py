92e76bbd312245bb1c9c270a82d5f9699a02e4ea
Stephen L Arnold <nerdboy@gentoo.org>
2022-06-24 23:26:56 -0700

    fix: dev: cleanup some lint in server and tox files
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- pyserv/server.py
+++ pyserv/server.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+"""
+Simple HTTP server with broken GET rewriting for serving FW upgrades.
+"""
+
 import logging
 import sys
-
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
-from pathlib import Path
 from urllib.parse import urlparse
 
 
@@ -15,31 +17,40 @@ class GetHandler(SimpleHTTPRequestHandler):
     Runs `urlparse` on the url and updates the GET handler path.
     """
     def do_GET(self):
-        host_str, file_path = parse_url(self.path)
-        self.path = file_path
-        logging.info(f'Path: {self.path}')
-        logging.info(f'Headers:\n{self.headers}\n')
+        logging.info('Path in: %s', self.path)
+        _, file_path = parse_url(self.path)
+        self.path = file_path  # pylint: disable=W0201
+        logging.info('Path out: %s', self.path)
+        logging.info('Headers:\n%s\n', self.headers)
         SimpleHTTPRequestHandler.do_GET(self)
 
 
 def parse_url(ota_url):
     """
     Parse the url sent by OTA command for file path and netloc.
+    :param ota_url: broken GET path if full url
+    :return tuple: netloc and path from ``urlparse``
     """
     get_data = urlparse(str(ota_url))
     file_path = get_data.path
     host_str = get_data.netloc
-    logging.debug("request file: {}".format(file_path.lstrip("/")))
-    logging.debug("request host: {}".format(host_str))
+    logging.debug('request file: %s', file_path.lstrip("/"))
+    logging.debug('request host: %s', host_str)
     return host_str, file_path
 
 
 def run(server_class=TCPServer, handler_class=GetHandler, port=8080):
-
+    """
+    Run command wrapper for console entry point; init logging and server,
+    run the server, stop the server.
+    :param server_class: should be TCPServer
+    :param handler_class: the GetHandler wrapper
+    :param port: server listen port
+    """
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    logging.info(f'Starting HTTP SERVER at PORT {port}')
+    logging.info('Starting HTTP SERVER at PORT %s', port)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -50,7 +61,7 @@ def run(server_class=TCPServer, handler_class=GetHandler, port=8080):
 
 
 def main(args=None):
-    """The main wrapper."""
+    """The entry point wrapper."""
     if args is None:
         args = sys.argv[1:]
 
