6c95c3bb4321329ff1eb41eda710f8f7aa59d8a9
Stephen L Arnold <nerdboy@gentoo.org>
2022-07-03 20:38:50 -0700

    fix: dev: packaging and lint cleanup, add damonizer deps
    * cleanup pylint and flake8 warnings, update setup.cfg and tox files
    * add daemon script dependencies to install_requires
    * install stand-alone httpdaemon script to venv bin dir
    * show both default paths and env values in settings display
    
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- pyserv/__init__.py
+++ pyserv/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from ._version import __version__
 
-version = __version__
+VERSION = __version__
 __description__ = "Simple HTTP server with GET rewriting and request/header logging."
 
-__all__ = ["__description__", "__version__", "version"]
+__all__ = ["__description__", "__version__", "VERSION"]
--- pyserv/settings.py
+++ pyserv/settings.py
@@ -2,12 +2,15 @@
 """
 Default settings for daemon mode.
 """
+import importlib
 import os
+import sys
 from pathlib import Path
 
 from appdirs import AppDirs
 
-from pyserv import version
+from pyserv import VERSION as version
+from pyserv import __description__  # noqa: F401
 
 
 def init_dirs(dirs):
@@ -17,9 +20,9 @@ def init_dirs(dirs):
 
     :param: list of Path objs
     """
-    for app_path in dirs:
-        if not app_path.exists():
-            app_path.mkdir(parents=True, exist_ok=True)
+    for usr_path in dirs:
+        if not usr_path.exists():
+            usr_path.mkdir(parents=True, exist_ok=True)
 
 
 def get_userdirs():
@@ -28,7 +31,7 @@ def get_userdirs():
 
     :return tuple: logdir, cachedir, docroot as Path objs
     """
-    dirs = AppDirs('pyserv', 'nerdboy')
+    dirs = AppDirs(appname='pyserv', version=version)
     logdir = Path(dirs.user_log_dir)
     cachedir = Path(dirs.user_cache_dir)
     docroot = Path(os.getcwd())
@@ -37,26 +40,41 @@ def get_userdirs():
 
 def show_uservars():
     """
-    Display user environment settings.
+    Display host platform user paths, files and environment.
     """
+    print("Python version:", sys.version)
+    print("-" * 79)
     print(f"pyserv {version}")
-    print("\nUser VARS:")
+
+    dirnames = ['log_dir', 'pid_dir', 'doc_root']
+    modname = 'pyserv.settings'
+    try:
+        mod = importlib.import_module(modname)
+        print(mod.__description__)
+
+        print("\nDefault user vars:")
+        for dirname, path in zip(dirnames, mod.get_userdirs()):
+            print(f'  {dirname}: {path}')
+
+    except (ImportError, AttributeError) as exc:
+        print("FAILED:", repr(exc))
+
+    print("\nCurrent environment values:")
     print(f"  DEBUG: {DEBUG}")
     print(f"  PORT: {PORT}")
     print(f"  IFACE: {IFACE}")
     print(f"  DOCROOT: {HOMEDIR}")
     print(f"  LOGFILE: {LOGFILE}")
     print(f"  PIDFILE: {PIDFILE}")
+    print("-" * 79)
 
 
-logdir, cachedir, docroot = get_userdirs()
-
-DEBUG = os.getenv('DEBUG', default=0)
+DEBUG = os.getenv('DEBUG', default='0')
 PORT = os.getenv('PORT', default='8080')
 IFACE = os.getenv('IFACE', default='127.0.0.1')
-HOMEDIR = os.getenv('DOCROOT', default=docroot)
-LOGFILE = os.getenv('LOGFILE', default=logdir.joinpath('httpd.log'))
-PIDFILE = os.getenv('PIDFILE', default=cachedir.joinpath('httpd.pid'))
+HOMEDIR = os.getenv('DOCROOT', default=str(get_userdirs()[2]))
+LOGFILE = os.getenv('LOGFILE', default=str(get_userdirs()[0].joinpath('httpd.log')))
+PIDFILE = os.getenv('PIDFILE', default=str(get_userdirs()[1].joinpath('httpd.pid')))
 LOGDIR = Path(LOGFILE).resolve().parent
 PIDDIR = Path(PIDFILE).resolve().parent
 
