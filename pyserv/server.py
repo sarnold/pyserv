f9af59fe7133baf1c8b92745d7f5554dea5d621a
Stephen L Arnold <nerdboy@gentoo.org>
2022-07-15 09:31:11 -0700

    fix: dev: remove one picky pylint warning
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- pyserv/server.py
+++ pyserv/server.py
@@ -9,7 +9,7 @@ import sys
 from . import GetServer
 from .settings import DEBUG
 
-lvl_name = 'DEBUG' if DEBUG else 'INFO'
+LVL_NAME = 'DEBUG' if DEBUG else 'INFO'
 
 
 def serv_init(iface, port, directory):
@@ -20,7 +20,7 @@ def serv_init(iface, port, directory):
     :param port: initialized listen port
     :return httpd_handler: threaded httpd handle, eg, httpd.start()
     """
-    logging.basicConfig(level=lvl_name)
+    logging.basicConfig(level=LVL_NAME)
     httpd_handler = GetServer(iface, port, directory)
     return httpd_handler
 
@@ -47,6 +47,7 @@ def main(args=None):  # pragma: no cover
     """
     The serv entry point wrapper; both args are optional, but you must
     provide either PORT or PORT *and* IFACE.
+
     Usage::
 
       serv [PORT] [IFACE]
